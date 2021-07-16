92854eb1cf9322f0d87ad2b63b07eed87410ae9e
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-07-16 03:16:37 +0100

    add contacts + appointments state hooks, and functions to update both with arrays of objects, and passed all this as props to components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  const addContact = (name, phone, email) => {
+    const newContact = {
+      name: name,
+      phone: phone,
+      email: email
+    };
+    setContacts((prev) => [...prev, newContact])
+  };
+
+  const addAppointment = (title, contact, date, time) => {
+    const newAppointment = {
+      title: title,
+      contact: contact,
+      date: date,
+      time: time
+    };
+    setAppointments((prev) => [...prev, newAppointment])
+  };
 
   return (
     <>
@@ -36,12 +48,17 @@ function App() {
             <Redirect to={ROUTES.CONTACTS} />
           </Route>
           <Route path={ROUTES.CONTACTS}>
-             {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage 
+              contacts={contacts} 
+              addContact={addContact}
+            />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
-            {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage 
+              appointments={appointments} 
+              contacts={contacts} 
+              addAppointment={addAppointment}
+            />
           </Route>
         </Switch>
       </main>
