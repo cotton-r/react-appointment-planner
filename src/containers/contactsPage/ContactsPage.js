7df3297e43e745595fb7037cae95d6b092edd1bc
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-07-17 17:34:16 +0100

    render a contact form and a TileList

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
-import ContactForm from '.../components/ContactForm/ContactForm';
-import TileList from '.../components/tileList/TileList';
-import { ProgressPlugin } from "webpack";
+
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from '../../components/tileList/TileList';
 
 export const ContactsPage = ({ contacts, addContact }) => {
   const [name, setName] = useState('');
@@ -35,21 +35,24 @@ export const ContactsPage = ({ contacts, addContact }) => {
     }
   }, [name, contacts, isDuplicate]);
 
-
-
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
-
   return (
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm 
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          onSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts}/>
       </section>
     </div>
   );
