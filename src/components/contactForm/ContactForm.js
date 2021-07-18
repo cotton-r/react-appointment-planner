2c0f6fb439fdbf4359dcae50df0689e907fa05d7
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-07-18 03:41:13 +0100

    add form to contactForm.js

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,9 +7,46 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  onSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={onSubmit}>
+      <label>
+        <input 
+          type="text" 
+          name="name" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder="Contact Name"
+        />
+      </label>
+      <br/>
+      <label>
+        <input 
+          type="tel" 
+          name="phone" 
+          value={phone} 
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          // regex is for UK phone numbers
+          pattern="^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$"
+          placeholder="Contact Phone (##### ######)"
+        />
+      </label>
+      <br/>
+      <label>
+        <input 
+          type="email" 
+          name="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Contact Email Address"
+        />
+      </label>
+      <br/>
+      <input type="submit" value="add contact" />
+    </form>
   );
 };
