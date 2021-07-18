071860337af91cf2144dd5e50e2d8d3bef015ceb
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-07-18 18:03:06 +0100

    add functionality to render a the props from each new contact as a tile

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-export const Tile = ({ object }) => {
+export const Tile = ({ tile }) => {
+
+  const tileFunc = () => {
+
+    const valuesArray = Object.values(tile);
+
+    const firstItem = () => {
+      if (this.key === 0) {
+        return "tile-title"
+      } else {
+        return "tile"
+      }
+    };
+
+    valuesArray.map((prop, index) => (
+      <p key={index} className={firstItem}>{prop}</p>
+    ));
+  };
+
   return (
     <div className="tile-container">
-      
+      {tileFunc}
     </div>
   );
 };
